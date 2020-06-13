8e568dbacce42bc68c8e4623fe1ea4529a8ffcde
siklylove <41222367+siklylove@users.noreply.github.com>
2020-06-13 14:31:58 +0800

    update

---
+++ back-end/GuGu_Server/src/Main/StartServers.java
@@ -0,0 +1,53 @@
+package Main;
+
+import DB.DBManager;
+import View.MainWindow;
+
+import java.awt.*;
+import java.sql.SQLException;
+
+public class StartServers {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainWindow frame = MainWindow.getMainWindow();
+                    DBManager dbManager = DBManager.getDBManager();
+
+                    ServerListener listener = new ServerListener();
+
+                    frame.setOnStartServersListener(new MainWindow.OnStartServersListener() {
+                        // Do something when servers stop
+                        @Override
+                        public void stop() {
+                            try {
+                                dbManager.getConnection().close();
+                                MainWindow.getMainWindow().setShowMsg("DB connection is closed");
+                            } catch (SQLException e) {
+                                MainWindow.getMainWindow().setShowMsg("DB connection close failed");
+                                e.printStackTrace();
+                            }
+                        }
+                        // Open servers
+                        @Override
+                        public void start() {
+                            dbManager.addDBDriver();
+                            dbManager.connectDB();
+                            try {
+                                dbManager.initDB();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            if (!listener.isAlive()) {
+                                listener.start();
+                            }
+                        }
+                    });
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}

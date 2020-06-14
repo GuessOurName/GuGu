b498cf98593c8c5e39b64391c52b82042dc5ab15
littlep1 <382399691@qq.com>
2020-06-15 02:21:53 +0800

    update

---
+++ back-end/GuGu_Server/src/Util/MomentMsg.java
@@ -0,0 +1,52 @@
+package Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MomentMsg {
+    private int iconID;
+    private String userId;
+    private String moment;
+    private int good;
+    private String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getIconID() {
+        return iconID;
+    }
+
+    public void setIconID(int iconID) {
+        this.iconID = iconID;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMoment() {
+        return moment;
+    }
+
+    public void setMoment(String moment) {
+        this.moment = moment;
+    }
+
+    public int getGood() {
+        return good;
+    }
+
+    public void setGood(int good) {
+        this.good = good;
+    }
+}
